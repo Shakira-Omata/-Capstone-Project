2451a4aff1979fefde84818e0ada8500602374d7
Shakira Omata <osajipurity3@gmail.com>
2025-04-02 15:07:13 +0300

    shak

---
+++ recipe-finder/src/pages/LoginPage.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const success = login(email, password);
+    if (success) {
+      navigate('/');
+    } else {
+      setError('Invalid email or password');
+    }
+  };
+
+  return (
+    <div 
+    className="flex flex-col items-center justify-center min-h-screen bg-purple-500 px-4"
+    style={{ 
+      backgroundImage: "url('/Cake.jpeg')",  
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+    }}
+  >
+        {/* Header */}
+      <div className="text-center mb-40 bg-black p-20  shadow-md  rounded-lg">  
+        <h1 className="text-4xl font-semibold text-white">Welcome to Recipe Finder</h1>
+        <p className="text-white font-semibold text-lg mt-2">Your<br></br> Ultimate Recipe Companion</p>
+      </div>
+
+      {/* Login Form */}
+      <div className="bg-white p-8 rounded-lg shadow-md w-96">
+        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+        {error && <p className="text-red-500 text-center mb-4">{error}</p>}
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label className="block text-gray-700">Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div>
+            <label className="block text-gray-700">Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-purple-500 text-white py-2 rounded-lg hover:bg-blue-600 transition"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
