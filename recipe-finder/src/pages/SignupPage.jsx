d1dfb4435ab1312925ce01c2b89218c30bd93d4e
Shakira Omata <osajipurity3@gmail.com>
2025-04-02 14:26:47 +0300

    shak

--- recipe-finder/src/pages/SignupPage.jsx
+++ recipe-finder/src/pages/SignupPage.jsx
@@ -27,7 +27,13 @@ const SignupPage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-purple-500">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-purple-500 px-4">
+        {/* Header */}
+      <div className="text-center mb-40 bg-black p-20  shadow-md  rounded-lg">  
+        <h1 className="text-4xl font-semibold text-white ">Welcome to Recipe Finder
+        </h1>
+        <p className="text-white font-semibold text-lg mt-2 ">Your<br></br> Ultimate Recipe Companion</p>
+      </div>
       <div className="bg-white p-8 rounded-lg shadow-md w-96">
         <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
         {error && <p className="text-red-500 text-center mb-4">{error}</p>}
