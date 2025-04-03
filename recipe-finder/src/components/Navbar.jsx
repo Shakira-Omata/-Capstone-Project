e783a424962375d215d0b98f4fccc2e6574d494c
Shakira Omata <osajipurity3@gmail.com>
2025-04-03 16:49:49 +0300

    shak

--- recipe-finder/src/components/Navbar.jsx
+++ recipe-finder/src/components/Navbar.jsx
@@ -8,6 +8,8 @@ const Navbar = () => {
         <div className="space-x-4">
           <Link to="/login" className="text-black font-bold hover:bg-white transition">Login</Link>
           <Link to="/signup" className="text-black font-bold hover:bg-black transition">Sign Up</Link>
+          <button onClick= {toggleDarkMode}
+           className="text-black font-bold hover:bg-white transition">Dark Mode</button>
         </div>
       </div>
     </nav>
