0657d8a52df2f050f6505c8c377d45cbde6ef2ea
Shakira Omata <osajipurity3@gmail.com>
2025-04-03 12:23:50 +0300

    shak

--- recipe-finder/src/components/Navbar.jsx
+++ recipe-finder/src/components/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/" className="text-white font-bold text-lg"></Link>
         <div className="space-x-4">
-          <Link to="/login" className="text-white font-bold">Login</Link>
-          <Link to="/signup" className="text-white font-bold">Sign Up</Link>
+          <Link to="/login" className="text-white font-bold hover:bg-black transition">Login</Link>
+          <Link to="/signup" className="text-white font-bold hover:bg-black transition">Sign Up</Link>
         </div>
       </div>
     </nav>
